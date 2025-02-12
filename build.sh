26d7f7f099d444de9a4d0379b49c311fa290a3ad
Phat Nguyen <ntp22880055@gmail.com>
2025-02-12 21:33:11 +0700

    hh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-echo "Cài đặt LibreOffice..."
+
+# Cài LibreOffice
 apt-get update && apt-get install -y libreoffice
+
+# Cài dependencies
+yarn install --frozen-lockfile
+
+# Chạy server
+yarn start
